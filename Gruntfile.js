5444578047a2af8737670866598eb141ae134d1b
Justin DuJardin <justin.dujardin@sococo.com>
2014-07-08 13:30:25 -0700

    chore(build): add grunt build and teamcity task support
     - generate artifacts in www/ for teamcity.
     - run harp server with default grunt task.  removes need for global harp install.

---
+++ Gruntfile.js
@@ -0,0 +1,87 @@
+var _ = require('underscore');
+
+module.exports = function(grunt) {
+
+   grunt.initConfig({
+      pkg: grunt.file.readJSON('package.json'),
+      clean: {
+         release: {
+            src: ["www/"]
+         }
+      },
+
+      copy: {
+         develop: {
+            files: [
+               { src: ['harp-develop.json'], dest: 'harp.json', filter: 'isFile'}
+            ]
+         },
+         release: {
+            files: [
+               { src: ['harp-release.json'], dest: 'harp.json', filter: 'isFile'}
+            ]
+         }
+      },
+
+
+      harp: {
+         develop: {
+            server: true,
+            source: ''
+         },
+         release: {
+            source: '',
+            dest: 'www'
+         }
+      },
+
+      /**
+       * Release/Deploy tasks
+       */
+      bump: {
+         options: {
+            files: ['package.json', 'bower.json', 'harp-develop.json', 'harp-release.json'],
+            updateConfigs: ['pkg'],
+            commit: true,
+            commitMessage: 'chore(deploy): release v%VERSION%',
+            commitFiles: ['package.json', 'bower.json', 'CHANGELOG.md'],
+            createTag: true,
+            tagName: 'v%VERSION%',
+            tagMessage: 'Version %VERSION%',
+            push: true,
+            pushTo: 'origin',
+            gitDescribeOptions: '--tags --always --abbrev=1 --dirty=-d'
+         }
+      },
+      changelog: {},
+
+      'npm-contributors': {
+         options: {
+            commitMessage: 'chore(attribution): update contributors'
+         }
+      }
+   });
+
+   grunt.loadNpmTasks('grunt-contrib-clean');
+   grunt.loadNpmTasks('grunt-contrib-copy');
+   grunt.loadNpmTasks('grunt-harp');
+
+   // Release/Deploy
+   grunt.loadNpmTasks('grunt-bump');
+   grunt.loadNpmTasks('grunt-conventional-changelog');
+   grunt.loadNpmTasks('grunt-npm');
+   grunt.registerTask('release', 'Build, bump and tag a new release.', function(type) {
+      type = type || 'patch';
+      grunt.task.run([
+         'npm-contributors',
+         "bump:" + type + ":bump-only",
+         'changelog',
+         'bump-commit'
+      ]);
+   });
+   // Default development task
+   grunt.registerTask('default', ['clean','copy:develop','harp:develop']);
+
+   // Build static site for release builds
+   grunt.registerTask('teamcity', ['clean','copy:release','harp:release']);
+};
